b3d4144b04ff382a8c47a1885f6988ee028dd66b
Roman Korotaev <kro1986@ukr.net>
2021-11-04 23:24:50 +0200

    Дописан функционал формы и поиска по слову

--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react";
-// import {useParams, NavLink, Route, useRouteMatch} from "react-router-dom" // возвращает объект динамических параметров ( то ято идут после двоеточия в   <Route path="/movies/:movieID"> в App.js)
+import {useParams, NavLink, Route, useRouteMatch} from "react-router-dom" // возвращает объект динамических параметров ( то ято идут после двоеточия в   <Route path="/movies/:movieID"> в App.js)
 import MovieService from '../../components/Services/MovieService';
 import s from './MoviesPage.module.css'
 import SearchForm from "../SearchForm/SearchForm";
@@ -13,20 +13,34 @@ const movieService = new MovieService();
 
 function MoviesPage () {
 
+    const {url} = useRouteMatch(); // берём url текущей страницы
+
     const [ quiryWord, setQuiryWord] = useState ("");
     const [ moviesArray, setMoviesArray] = useState ([]);
 
 
 
-    movieService.getMovieQuery("bond")
-    .then (res => {
-        console.log ("СПИСОК НАЙДЕННЫХ ФИЛЬМОВ : ", res)
-        console.log ("СПИСОК НАЙДЕННЫХ ФИЛЬМОВ - ПОДРОБНО : ", res.data.results)
-        setMoviesArray([...res.data.results])
-    });
+   
 
+     useEffect ( ()=> { 
 
-    
+        if ( quiryWord!=="") {
+        movieService.getMovieQuery(quiryWord)
+            .then (res => {
+                console.log ("СПИСОК НАЙДЕННЫХ ФИЛЬМОВ : ", res)
+                console.log ("СПИСОК НАЙДЕННЫХ ФИЛЬМОВ - ПОДРОБНО : ", res.data.results)
+                    if (res.data.results.length !== 0) { 
+                        setMoviesArray([...res.data.results]) // ВАЖНО СИНТАКСИС: именно так в данной функции записываем массив
+                        console.log (" Записали hits  в   -  moviesArray через хуки (аналог componentDidUpdate )",  moviesArray );
+                    }  
+            })
+            .catch(() => {
+                alert("Something wrong. Please try again later");
+              })
+
+            }
+
+       }, [quiryWord] )
 
 
 
@@ -35,18 +49,22 @@ function MoviesPage () {
         setQuiryWord (quiryWord) ;
       }
 
-    
+      console.log ("quiryWord = ", quiryWord)
+
+
           
     return (
+
         <div>
                 <p> компонент MoviesPage, страница поиска фильмов по ключевому слову.</p>
                 < SearchForm onFormSubmit= {handleSubmitForm} />
 
-                <ul>
-                    { moviesArray.map ( oneMovie => (
-                        <li>
-                            Movie: { oneMovie.original_title}
-                            </li>
+                <ul>hgfjhfhjf
+                    { moviesArray.map ( ({id, original_title}) => (
+                        <li key = {id}>
+                            Movie: {original_title}
+                            {/* <NavLink to={  `{url}/${id}` }> {original_title} </NavLink> */}
+                        </li>
                     ))
 
                     }
